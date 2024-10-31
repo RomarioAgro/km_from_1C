22b4e0b3909f7295707a2fc85e0c6c860e401f3e
Кожин Роман <resume-gl@yandex.ru>
2024-10-31 11:58:58 +0300

    сделал сохранение в файл кода артикула, чтоб потом сбис мог прочитать и привязать к нему КМ

--- newmark.py
+++ newmark.py
@@ -5,9 +5,11 @@ from sys import argv, exit
 import logging
 import datetime
 import os
+PATH_LOG = 'd:\\files\\'
+
 
 logging.basicConfig(
-    filename='d:\\files\\' + os.path.basename(__file__)[:-3] + '_' + datetime.date.today().strftime('%Y-%m-%d') + '.log',
+    filename=PATH_LOG + os.path.basename(__file__)[:-3] + '_' + datetime.date.today().strftime('%Y-%m-%d') + '.log',
     filemode='a',
     level=logging.DEBUG,
     format="%(asctime)s - %(filename)s - %(funcName)s: %(lineno)d - %(message)s",
@@ -15,6 +17,7 @@ logging.basicConfig(
 
 # Создание объекта конфигурации
 config = Config(RepositoryEnv(os.path.dirname(os.path.abspath(__file__)) + '\\mark.env'))
+
 
 class KM_1C:
     def __init__(self, f_path):
@@ -38,7 +41,10 @@ class KM_1C:
             'Content-Type': 'application/json',
             'Authorization': 'Basic ' + self.token
         }
-        r = requests.post(url=url, headers=headers, json=self.km_dict)
+        try:
+            r = requests.post(url=url, headers=headers, json=self.km_dict)
+        except Exception as exc:
+            logging.debug(f"ошибка запроса нового о КМ {exc}")
         logging.debug(f"результат запроса нового КМ {r.text}")
 
 
@@ -47,13 +53,32 @@ class KM_1C:
         запрос в 1С о валидности КМ, какому товару принадлежит КМ
         :return:
         """
-        url = self.base_url + f'/infomark/{self.km_dict.get("skl_id")}/'
+        url = self.base_url + f'/infomark/{self.km_dict.get("skl_id")}'
         headers = {
             'Content-Type': 'application/json',
             'Authorization': 'Basic ' + config('token_1C')
         }
-        r = requests.post(url=url, headers=headers, json=self.km_dict)
-        logging.debug(f"результат запроса инфо о КМ {r.text}")
+        km = self.km_dict.get('sost')[0].get("code")
+        param = {
+            "code": km
+        }
+        logging.debug(f"json = {param}")
+        decoded_text = 'пока ошибок нет'
+        try:
+            r = requests.post(url=url, headers=headers, json=param)
+            decoded_text = r.content.decode('utf-8')
+        except Exception as exc:
+            logging.debug(f"ошибка запроса инфо о КМ {exc} {decoded_text}")
+            return 401
+        print(decoded_text)
+        file_name = os.path.splitext(os.path.basename(argv[1]))[0]
+        logging.debug(f"результат запроса инфо о КМ {decoded_text}")
+        if r.status_code == 200:
+            with open(PATH_LOG + file_name + '_answer.txt', 'w') as f_km:
+                f_km.write(r.text)
+        return r.status_code
+
+
 
 
 def main():
@@ -61,7 +86,7 @@ def main():
     if argv[2] == 'new':
         mark.new_mark()
     if argv[2] == 'info':
-        mark.info_mark()
+        exit(mark.info_mark())
 
 
 if __name__ == '__main__':
