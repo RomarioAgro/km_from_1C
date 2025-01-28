279be37f9f01f4c1366c62ffef0b816d70bb9840
Кожин Роман <resume-gl@yandex.ru>
2025-01-28 12:22:22 +0300

    добавил постраничный импорт КМ из 1С, когда их много, например несколько десятков тысяч

--- newmark.py
+++ newmark.py
@@ -5,6 +5,7 @@ from sys import argv, exit
 import logging
 import datetime
 import os
+import re
 PATH_LOG = 'd:\\files\\'
 
 
@@ -78,6 +79,80 @@ class KM_1C:
                 f_km.write(r.text)
         return r.status_code
 
+    def freemarkcontent(self):
+        """
+        есть некий пул КМ которые не распечатаны
+        запрос за списком страниц этого пула, их статус в сбис, загружены или нет
+        :return: int
+        """
+        url = self.base_url + f'/freemarkcontent/{self.km_dict.get("skl_id")}'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + config('token_1C')
+        }
+        decoded_text = 'пока ошибок нет'
+        try:
+            r = requests.get(url=url, headers=headers)
+            decoded_text = r.content.decode('utf-8')
+        except Exception as exc:
+            logging.debug(f"ошибка запроса об пул км {exc} {decoded_text}")
+            return 401
+        print(decoded_text)
+        file_name = os.path.splitext(os.path.basename(argv[1]))[0]
+        logging.debug(f"результат запроса инфо пул км {decoded_text}")
+        if r.status_code == 200:
+            ids_with_false_status = list(map(lambda item: item['id'], filter(lambda item: item['uploaded'] is False, r.json())))
+            with open(PATH_LOG + file_name + '.txt', 'w') as f_lp:
+                f_lp.write(','.join(ids_with_false_status))
+        return r.status_code
+
+    def freemarkcontent_getpage(self, page: str = '1'):
+        """
+        получение содержимого пула свободных КМ по номеру страницы
+        :return:
+        """
+        url = self.base_url + f'/freemark/{self.km_dict.get("skl_id")}/{page}'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + config('token_1C')
+        }
+        decoded_text = 'пока ошибок нет'
+        try:
+            r = requests.get(url=url, headers=headers)
+            decoded_text = r.content.decode('utf-8')
+        except Exception as exc:
+            logging.debug(f"ошибка запроса состава страницы пула км  {exc} {decoded_text}")
+            return 401
+        file_name = os.path.splitext(os.path.basename(argv[1]))[0]
+        logging.debug(f"результат запроса состава страницы пула км {decoded_text}")
+        if r.status_code == 200:
+            new_data = [(item['barcod'], item['markFull']) for item in r.json()]
+            with open(PATH_LOG + file_name + '_page_' + page + '.txt', 'w') as f_km:
+                for elem in new_data:
+                    mrk = re.sub(r"[\x1D]", "", elem[1])  # \x1D - шестнадцатеричное представление ASCII 29
+                    f_km.write(f'{elem[0]}delimeter{mrk}\n')
+        return r.status_code
+
+    def freemarkcontent_closepage(self, page: str = '1'):
+        """
+        закрытие страницы с КМ из пула свободных
+        :return:
+        """
+        url = self.base_url + f'/freemark/{self.km_dict.get("skl_id")}/{page}'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + config('token_1C')
+        }
+        decoded_text = 'пока ошибок нет'
+        try:
+            r = requests.put(url=url, headers=headers)
+            decoded_text = r.content.decode('utf-8')
+        except Exception as exc:
+            logging.debug(f"ошибка запроса закрытия страницы пула км  {exc} {decoded_text}")
+            return 401
+        logging.debug(f"результат запроса закрытия страницы пула км {decoded_text}")
+        print(r.text)
+        return r.status_code
 
 
 
@@ -87,6 +162,13 @@ def main():
         mark.new_mark()
     if argv[2] == 'info':
         exit(mark.info_mark())
+    if argv[2] == 'freepoollistpage':
+        mark.freemarkcontent()
+    if argv[2] == 'freepoolgetpage':
+        mark.freemarkcontent_getpage(page=mark.km_dict.get('page', None))
+    if argv[2] == 'freepoolclosepage':
+        mark.freemarkcontent_closepage(page=mark.km_dict.get('page', None))
+
 
 
 if __name__ == '__main__':
